10e961212e0892e953a8e41e5bcba18cfc24406a
virajsazzala <vir200319@gmail.com>
2023-03-31 17:25:08 +0530

    fixed spelling mistake

--- projects/urls.py
+++ projects/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # ex: /projects/3/
     path('<int:project_id>/', views.detail, name='detail'),
     # ex: /projects/3/members/
-    path('<int:project_id>/members/', views.members, name='memebers'),
+    path('<int:project_id>/members/', views.members, name='members'),
     # ex: /projects/3/partake/
     path('<int:project_id>/partake/', views.partake, name='partake'),
 ]
